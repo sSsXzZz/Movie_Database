25c5456f3f9bcbd5a35c7ea775ec256e4bfad04c
SSY <shalinpatel75@gmail.com>
2016-11-19 15:33:28 -0500

    Setup index page to show all movies sorted by name

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    db.get().query("SELECT movie_title FROM Movies ORDER BY movie_title", function (err, rows, fields){
+        if (err) throw err;
+        var movies = rows.map(function(movie) {
+            return movie.movie_title;
+        });
+        res.render('index', { title: 'Express', movies: movies });
+    });
 });
 
 module.exports = router;
