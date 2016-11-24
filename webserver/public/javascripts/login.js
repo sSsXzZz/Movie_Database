da788cad1523a17362496d52098bc60fc97437ab
SSY <shalinpatel75@gmail.com>
2016-11-23 23:19:03 -0500

    Added logic for handling login with the input fields and creating the uid cookie

--- webserver/public/javascripts/login.js
+++ webserver/public/javascripts/login.js
@@ -1,21 +1,33 @@
-
 $(document).ready(function(){
+    // on login attempt send ajax request
     $("#login_request_button").on('click',  function(){
         $.ajax({
             url: "users/login",
             type: "POST",
             data: {
-                username: "yash",
-                password: "notthepassword",
+                username: $("#username_textfield").val(),
+                password: $("#password_textfield").val(),
             },
             success: login_success,
             error: login_error
         });
     });
+    $("#username_textfield,#password_textfield").keypress(function(e){
+        if (e.keyCode ===13){
+            $("#login_request_button").click();
+        }
+    });
+
+    if (typeof Cookies.get('uid') !== "undefined"){
+        $("#login_button").text("Account");
+        $("#login_button").attr("href","account_link");
+    }
 });
 
 function login_success(data, textStatus, jqXHR){
-    console.log("WE DO LOGIN SHIT NOW");
+    // the responseText should contain the uid
+    Cookies.set('uid',jqXHR.responseText);
+    location.reload();
 }
 
 function login_error(jqXHR, textStatus, errorThrown){
