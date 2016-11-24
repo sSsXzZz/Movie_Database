8f803af996941c58825fa70460ca9478e409e38a
Sahil Patel <sahpat229@gmail.com>
2016-11-24 17:24:24 -0500

    User Sidebar

---
+++ webserver/public/javascripts/user_sidebar.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    $(".user_sidebar").css('width', '20%');
+    document.body.style.backgroundColor = "rgba(0,0,0,0.8)";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    $(".user_sidebar").css('width', '0%');
+    document.body.style.backgroundColor = "white";
+}
