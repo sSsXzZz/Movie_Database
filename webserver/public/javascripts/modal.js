1ff523456525fdb494ea02b5e935cf9e1d5fd75d
SSY <shalinpatel75@gmail.com>
2016-11-27 17:37:59 -0500

    Changed button handler for login modal to use jquery selection

--- webserver/public/javascripts/modal.js
+++ webserver/public/javascripts/modal.js
@@ -2,16 +2,13 @@ $(document).ready(function(){
   // Get the modal
   var modal = document.getElementById('modal1');
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("login_button");
-
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
   // When the user clicks the button, open the modal
-  btn.onclick = function() {
+  $("#login_button").on('click', function() {
       modal.style.display = "block";
-  }
+  });
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
@@ -79,4 +76,3 @@ $(document).ready(function(){
       }
   }
 });
-
