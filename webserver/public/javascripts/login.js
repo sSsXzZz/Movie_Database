0911a7611f294aab55cc06e2f0e39fcac143c7ca
SSY <shalinpatel75@gmail.com>
2016-11-27 18:48:56 -0500

    Setup post request on frontend for signing up

--- webserver/public/javascripts/login.js
+++ webserver/public/javascripts/login.js
@@ -12,25 +12,41 @@ $(document).ready(function(){
             error: loginError
         });
     });
+
+    $("#signup_request_button").on('click',  function(){
+        $.ajax({
+            url: "users/signup",
+            type: "POST",
+            data: {
+                username: $("#username_textfield").val(),
+                password: $("#password_textfield").val(),
+            },
+            success: loginSuccess,
+            error: loginError
+        });
+    });
+
     $("#username_textfield,#password_textfield").keypress(function(e){
         if (e.keyCode ===13){
             $("#login_request_button").click();
         }
     });
 
+    // cookie found so logged in
     if (typeof Cookies.get('uid') !== "undefined"){
         changeLoginToLogout();
     }
 
+    // logout
     $("#logout_button").on('click', function(){
         Cookies.remove('uid');
+        location.reload();
     });
 });
 
 function loginSuccess(data, textStatus, jqXHR){
     // the responseText should contain the uid
-    Cookies.set('uid',jqXHR.responseText);
-    changeLoginToLogout();
+    Cookies.set('uid',jqXHR.responseText, {expires: 1});
     location.reload();
 }
 
