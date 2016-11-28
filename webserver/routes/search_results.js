8cd5fdc4713bfdddd4244fdde14f154bf7a6fa21
SSY <shalinpatel75@gmail.com>
2016-11-27 23:26:14 -0500

    Setup search results to pull in data dynamically

--- webserver/routes/search_results.js
+++ webserver/routes/search_results.js
@@ -4,10 +4,12 @@ var db = require('../db');
 
 router.get('/', function(req, res, next) {
     var multiple_tables=false; //flag for if we are querying multiple tables
+    var valid_query = false;
     var sql_query;
     sql_query = "SELECT * FROM ( ";
     for ( var param in req.query){
         if ( (param==="Movies" || param==="Actors" || param ==="Directors") && req.query[param] ==='true'){
+            valid_query=true;
             var name_field = (param ==="Movies" ? "movie_title" : "name");
             if (!multiple_tables){
                 sql_query += "SELECT "
@@ -39,10 +41,14 @@ router.get('/', function(req, res, next) {
         }
     }
     sql_query += " ) AS search_results ORDER BY name";
-    db.get().query(sql_query, function (err, rows, fields){
-        if (err) throw err;
-        res.render('search_results', {title: 'Search Results', entries: rows});
-    });
+    if(valid_query){
+        db.get().query(sql_query, function (err, rows, fields){
+            if (err) throw err;
+            res.render('search_results', {title: 'Search Results', entries: rows});
+        });
+    } else {
+        res.status(400).send("Invalid search request!");
+    }
 });
 
 module.exports = router;
