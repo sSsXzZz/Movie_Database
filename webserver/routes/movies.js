5d25657b5aa57aff25ece16984e9cad16dda46d9
SSY <shalinpatel75@gmail.com>
2016-11-27 22:49:14 -0500

    Setup movie details dynamic page

--- webserver/routes/movies.js
+++ webserver/routes/movies.js
@@ -2,14 +2,17 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-/*
-router.get('/', function(req, res, next) {
-    res.render('movie_detail.ejs');
-});
-*/
-
-router.get('/a', function(req, res, next) {
-    res.render('movie_detail.ejs');
+router.get("/:key", function(req, res, next) {
+    var moviekey = parseInt(req.params.key, 10);
+    if (!Number.isInteger(moviekey)){
+        res.status(400).send("Requested movie does not exist!");
+        return;
+    }
+    var query_string = "SELECT * FROM Movies WHERE mid=" + moviekey;
+    db.get().query(query_string,function(err,rows, fields){
+        if (err) throw err;
+        res.render('movie_detail.ejs', {data: rows[0] });
+    });
 });
 
 module.exports = router;
