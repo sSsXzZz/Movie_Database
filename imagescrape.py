0d0535e49f4999c496d2f49a9c3ed583ca7a3a69
ysharma1126 <ysharma1126@gmail.com>
2016-12-01 03:00:31 -0500

    Update imagescrape.py

--- imagescrape.py
+++ imagescrape.py
@@ -1,46 +1,59 @@
-import httplib, urllib, base64
+mport httplib, urllib, base64
 import json
 import MySQLdb
+import time
 
 db = MySQLdb.connect("localhost","root","vishnu1","imdb")
 x = db.cursor()
 
-sql = "SELECT movie_title FROM Movies"
+sql_query = "SELECT movie_title FROM Movies"
 
 headers = {
-    # Request headers
-    #28ce2731d0af4e85a5f60f3b6ba2edf5
-    'Ocp-Apim-Subscription-Key': 'c78e761f28224c789c98d5a60939c8a4',
+  # Request headers
+  #4993
+  'Ocp-Apim-Subscription-Key': '2eedf395659f4e098ff62a037897049f',
 }
 
 
 try:
-    x.execute(sql)
-    result = x.fetchall()
-    i = 0
-    for row in result:
-        params = urllib.urlencode({
-        # Request parameters
-        'q': str(row),
-        'count': '1',
-        'offset': '0',
-        'mkt': 'en-us',
-        'safeSearch': 'Moderate',
-        })
-        if (i < 975):
-            try:
-                i=i+1
-                conn = httplib.HTTPSConnection('api.cognitive.microsoft.com')
-                conn.request("GET", "/bing/v5.0/images/search?%s" % params, "{body}", headers)
-                response = conn.getresponse()
-                data = response.read()
-                data_json  = json.loads(data)
-                #data_json['value'][0][u'contentURL]
-                #Run INSERT if Sable allows
-            except Exception as e:
-                print("[Errno {0}] {1}".format(e.errno, e.strerror))
-    #Check if we pass the API Limit
-    print i
+  x.execute(sql_query)
+  result = x.fetchall()
+  i = 0
+  for row in result:
+    i = i+1
+    if (i > 4796):
+      print i
+      param = str(row)
+      param = param[2:-3]
+      param = param + ' movie cover'
+      params = urllib.urlencode({
+      'q': param,
+      'count': '1',
+      'offset': '0',
+      'mkt': 'en-us',
+      'safeSearch': 'Moderate',
+      })
+      print param
+      try:
+        conn = httplib.HTTPSConnection('api.cognitive.microsoft.com')
+        conn.request("GET", "/bing/v5.0/images/search?%s" % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        data_json  = json.loads(data)
+        url = data_json['value'][0]['contentUrl']
+        print url
+        sql_insert = "UPDATE Movies SET image_url=%s WHERE mid=%s "
+        sql_data = (url,i)
+        try:
+          x.execute(sql_insert,sql_data)
+          db.commit()
+        except:
+          print 'cheese'
+          db.rollback()
+      except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+      #time.sleep(5)
 except:
-    print "Error: unable to fetch data"
+  print "Error: unable to fetch data"
 db.close()
+
