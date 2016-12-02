d8c17bccb4affba5863228c4e5a03aa003beddf8
SSY <shalinpatel75@gmail.com>
2016-12-02 14:03:34 -0500

    Setup actor and director names

--- webserver/routes/movies.js
+++ webserver/routes/movies.js
@@ -8,10 +8,14 @@ router.get("/:key", function(req, res, next) {
         res.status(400).send("Requested movie does not exist!");
         return;
     }
-    var query_string = "SELECT * FROM Movies WHERE mid=" + moviekey;
+    var query_string = "SELECT M.*, A.name as \"actor_name\", D.name as \"director_name\" " 
+    + "FROM Movies M, Actors A, Actor_Movie AM, Directors D, Director_Movie DM " 
+    + "WHERE M.mid=" + moviekey + " AND AM.mid=" + moviekey + " AND A.aid=AM.aid" + " AND DM.mid=" + moviekey + " AND D.did=DM.did";
     db.get().query(query_string,function(err,rows, fields){
         if (err) throw err;
-        res.render('movie_detail.ejs', {data: rows[0] });
+        console.log(rows);
+        var actors = rows.map(function(a) { return a.actor_name });
+        res.render('movie_detail.ejs', {data: rows[0], actors: actors, director: rows[0].director_name });
     });
 });
 
