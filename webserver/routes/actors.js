5653e9fb1631f7d0bf9728b6891fda3d56fff761
SSY <shalinpatel75@gmail.com>
2016-12-03 00:50:55 -0500

    Added dynamic content and rating/comments to actor details

--- webserver/routes/actors.js
+++ webserver/routes/actors.js
@@ -8,34 +8,42 @@ router.get("/:key", function(req, res, next) {
         res.status(400).send("Requested movie does not exist!");
         return;
     }
-    var query_string = "SELECT M.*, A.name as \"actor_name\", D.name as \"director_name\", G.name as \"genre\", K.keyword " 
-    + "FROM Movies M, Actors A, Actor_Movie AM, Directors D, Director_Movie DM, Genres G, Movie_Keywords K " 
-    + "WHERE M.mid=" + moviekey + " AND AM.mid=" + moviekey + " AND A.aid=AM.aid" + " AND DM.mid=" + moviekey + " AND D.did=DM.did" + " AND G.mid=" + moviekey + " AND K.mid=" + moviekey;
+    var query_string = "SELECT A.*, M.movie_title, M.mid, M.image_url as \"movie_url\", D.name as \"director_name\", D.did, D.image_url as \"director_url\""
+        + " FROM Directors D, Movies M, Actors A, Director_Movie DM, Actor_Movie AM"
+        + " WHERE A.aid=" + req.params.key + " AND AM.aid=" + req.params.key + " AND AM.mid=M.mid AND DM.mid=AM.mid AND D.did=DM.did";
     db.get().query(query_string,function(err,rows, fields){
         if (err) throw err;
-        //var actors = rows.map(function(a) { return a.actor_name });
-        var actors = [];
-        var genres = [];
-        var keywords = [];
+        var directors = [];
+        var movies = [];
         rows.forEach(function(entry){
-            if (actors.indexOf(entry.actor_name) === -1){
-                actors.push(entry.actor_name);
+            if (objectArrayIndexOf(directors,entry.did,"did") === -1){
+                directors.push({
+                    name: entry.director_name,
+                    did: entry.did,
+                    image_url: entry.director_url
+                });
             }
-            if (genres.indexOf(entry.genre) === -1){
-                genres.push(entry.genre);
-            }
-            if (keywords.indexOf(entry.keyword) === -1){
-                keywords.push(entry.keyword);
+            if (objectArrayIndexOf(movies,entry.mid,"mid") === -1){
+                movies.push({
+                    movie_title: entry.movie_title,
+                    mid: entry.mid,
+                    image_url: entry.movie_url,
+                });
             }
         });
         res.render('actor_detail.ejs', {
             data: rows[0],
-            actors: actors,
-            director: rows[0].director_name,
-            genres: genres,
-            keywords: keywords,
+            directors: directors,
+            movies: movies,
         });
     });
 });
+
+function objectArrayIndexOf(myArray, searchTerm, property) {
+    for(var i = 0, len = myArray.length; i < len; i++) {
+        if (myArray[i][property] === searchTerm) return i;
+    }
+    return -1;
+}
 
 module.exports = router;
