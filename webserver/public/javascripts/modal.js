57cd34c754df9b53ad975e2e7119c45dd0f61018
Sahil Patel <sahpat229@gmail.com>
2016-12-16 20:43:21 -0500

    m

--- webserver/public/javascripts/modal.js
+++ webserver/public/javascripts/modal.js
@@ -60,12 +60,12 @@ $(document).ready(function(){
   // When the user clicks the button, open the modal
   hist_btn.onclick = function() {
       hist_modal.style.display = "block";
-  }
+  };
 
   // When the user clicks on <span> (x), close the modal
   hist_span.onclick = function() {
       hist_modal.style.display = "none";
-  }
+  };
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
@@ -76,3 +76,39 @@ $(document).ready(function(){
       }
   }
 });
+
+
+
+  // Get the history modal
+  var sup_modal = document.getElementById('superuser_modal');
+
+  // Get the button that opens the modal
+  var sup_btn = document.getElementById("superuser_button");
+
+  // Get the <span> element that closes the modal
+  var sup_span = document.getElementById("superuser_close");
+
+  // When the user clicks the button, open the modal
+  sup_btn.onclick = function() {
+      sup_modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  sup_span.onclick = function() {
+      sup_modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if ((event.target == hist_modal) || (event.target == set_modal) || (event.target == modal) || (event.target == sup_modal)) {
+          hist_modal.style.display = "none";
+          set_modal.style.display = "none";
+          modal.style.display = "none";
+          sup_modal.style.display = "none";
+      }
+  };
+
+
+
+
+
