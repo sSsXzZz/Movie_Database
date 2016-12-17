6503a5e2e933dfa52bda99438ff1f7bd02b31208
Sahil Patel <sahpat229@gmail.com>
2016-12-16 19:18:56 -0500

    u

--- webserver/public/javascripts/update.js
+++ webserver/public/javascripts/update.js
@@ -46,61 +46,68 @@
 //     $('.owl-wrapper-outer').css('height', 220 + 'px');
 // }
 
-function updateSize(){
-    var minHeight = 300;
+// function updateSize(){
+//     var minHeight = 300;
 
-    var count = 0;
+//     var count = 0;
 
-    $('.owl-item').each(function () {
-        if ($(this).find('img').hasClass('blank'))
-        {
-            return;
-        }
-        var thisHeight = parseInt($(this).find('img').css('height'));
-        console.log(count + ',' + thisHeight);
-        count += 1;
-        if (thisHeight < 100)
-        {
-          return;
-        }
-        minHeight = (minHeight <= thisHeight ? minHeight:thisHeight);
-    });
+//     $('.owl-item').each(function () {
+//         if ($(this).find('img').hasClass('blank'))
+//         {
+//             return;
+//         }
+//         var thisHeight = parseInt($(this).find('img').css('height'));
+//         console.log(count + ',' + thisHeight);
+//         count += 1;
+//         if (thisHeight < 100)
+//         {
+//           return;
+//         }
+//         minHeight = (minHeight <= thisHeight ? minHeight:thisHeight);
+//     });
 
-    var maxHeight = minHeight;
+//     var maxHeight = minHeight;
+
+//     $('.owl-item').each(function(){
+        
+//         var pict = $(this).find('img');
+
+//         var ratio = 0;
+//         var width = pict.width();
+//         var height = pict.height();
+//         console.log("Orig Width: " + width + "Orig Height: " + height);
+//         if (height > maxHeight){
+//             ratio = maxHeight / height;
+//             pict.css("height", maxHeight);
+//             pict.css("width", width* ratio);
+//             width = width * ratio;
+//             height = height * ratio;
+//         }
+
+//         if (height < maxHeight)
+//         {
+//             ratio = maxHeight / height;
+//             pict.css("height", maxHeight);
+//             pict.css("width", width*ratio);
+//             width = width*ratio;
+//             height = height * ratio;
+//         }
+
+//         var nw = pict.width();
+//         var nh = pict.height();
+//         $(this).css('height', minHeight+'px');
+//         console.log("New width: " + nw + "New Height: " + nh);
+//     });
+
+
+//     minHeight += 20;
+//     $('.owl-wrapper-outer').css('height', minHeight+'px');
+//     console.log(minHeight);
+// }
+
+function updateSize() {
 
     $('.owl-item').each(function(){
-        
-        var pict = $(this).find('img');
-
-        var ratio = 0;
-        var width = pict.width();
-        var height = pict.height();
-        console.log("Orig Width: " + width + "Orig Height: " + height);
-        if (height > maxHeight){
-            ratio = maxHeight / height;
-            pict.css("height", maxHeight);
-            pict.css("width", width* ratio);
-            width = width * ratio;
-            height = height * ratio;
-        }
-
-        if (height < maxHeight)
-        {
-            ratio = maxHeight / height;
-            pict.css("height", maxHeight);
-            pict.css("width", width*ratio);
-            width = width*ratio;
-            height = height * ratio;
-        }
-
-        var nw = pict.width();
-        var nh = pict.height();
-        $(this).css('height', minHeight+'px');
-        console.log("New width: " + nw + "New Height: " + nh);
+        $(this).removeProperty('width');
     });
-
-
-    minHeight += 20;
-    $('.owl-wrapper-outer').css('height', minHeight+'px');
-    console.log(minHeight);
 }
